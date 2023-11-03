5b975f837024cae663263bf3bab0eba15f07bbb9
Tannez <92729005+Tannez@users.noreply.github.com>
2023-11-03 15:30:39 +0100

    Tinkering with levelChanger script

--- Assets/Transition Animations/LevelChanger.cs
+++ Assets/Transition Animations/LevelChanger.cs
@@ -5,23 +5,109 @@ using UnityEngine.SceneManagement;
 
 public class LevelChanger : MonoBehaviour
 {
+    // bool to unlock test conditions
+    [SerializeField] bool ReloadSceneTest = true;
+    [SerializeField] bool NewSceneTest = true;
+
     // Call for animator object in Unity (insert the animator called 'LevelChanger' into inspector)
     public Animator animator;
 
     // Variable for loading certain level (based on level index)
     private int levelToLoad;
 
+    //Determine current scene
+        [HideInInspector] public bool Scene1Active = true;
+            // Catch bools from other script
+                //StonesLoaded;
+                //WoodChopped;
+
+        [HideInInspector] public bool Scene2Active = false;
+            // Catch bools from other script
+                //ItemGathered;
+
+        [HideInInspector] public bool Scene3Active = false;
+            // Catch bools from other script
+                //StonesPlaced;
+                //TorchLit;
+
+    void Awake()
+    {
+        // This condition only applies when testing.
+        if (ReloadSceneTest == true || NewSceneTest == true)
+        {
+            Scene1Active = false;
+            Scene2Active = false;
+            Scene3Active = false;
+        }
+
+        // Debug.Log("Scene 1 Is Now Active!");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // These conditions only apply when testing. 
+        if (Input.GetMouseButtonDown(0) && ReloadSceneTest == true && NewSceneTest == false)
         {
             FadeToLevel(1);
+            Debug.Log("Reloading Same Scene!");
         }
+
+        if (Input.GetMouseButtonDown(0) && NewSceneTest == true && ReloadSceneTest == false)
+        {
+            FadeToNextLevel();
+            Debug.Log("Entering New Scene!");
+        }
+
+        else if (NewSceneTest == true && ReloadSceneTest == true && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Can't Reload And Start New Scene At The Same Time!");
+        }
+
+
+        // Conditions for interactive experience
+        /*
+        if (Scene1Active == true)
+        {
+            if(StonesLoaded == true && WoodChopped == true)
+            {
+                FadeToNextLevel();
+                Scene2Active = true;
+                Scene1Active = false;
+                Debug.Log("Scene 2 Is Now Active!");
+            }
+        }
+
+        if (Scene2Active == true)
+        {
+            if(ItemGathered == true)
+            {
+                FadeToNextLevel();
+                Scene3Active = true;
+                Scene2Active = false;
+                Debug.Log("Scene 3 Is Now Active!");
+            }
+        }
+
+        if (Scene3Active == true)
+        {
+            if(StonesPlaced == true && TorchLit == true)
+            {
+                FadeToNextLevel();
+                Debug.Log("Experience Is Over!");
+            }
+        }
+        
+        */
     }
 
-    // Method for running animation and setting level to load.
-    public void FadeToLevel (int levelIndex)
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Method for running animation and setting the level index to load.
+    public void FadeToLevel(int levelIndex)
     {
         levelToLoad = levelIndex;
         animator.SetTrigger("FadeOut");
