50f2e47a7737d8ac7bcba9bfe6632594a292494b
Tannez <92729005+Tannez@users.noreply.github.com>
2023-11-10 14:57:59 +0100

    Wood can be gathered, unlocking bool in LevelChanger

---
+++ Assets/GatherWood.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatherWood : MonoBehaviour
+{
+    public LevelChanger levelChanger;
+    public int GatheredWood = 0; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelChanger = GameObject.Find("LevelChanger").GetComponent<LevelChanger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GatheredWood == 6)
+        {
+            levelChanger.WoodStacked = true;
+        }
+
+        else
+        {
+            levelChanger.WoodStacked = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ChoppedLog"))
+        {
+            GatheredWood += 1;
+        }
+
+        else if (other.CompareTag("WoodLog"))
+        {
+            Debug.Log("Wood Has Not Been Chopped!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ChoppedLog"))
+        {
+            GatheredWood -= 1;
+        }
+    }
+}
