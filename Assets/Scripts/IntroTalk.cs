8a8853f820b34c21bbfee6f1dd637cb5352ea765
RasmusKanne <78956812+RasmusKanne@users.noreply.github.com>
2023-12-10 17:02:53 +0100

    Intro Talk + Small Scripted Changes
    A 4 seconds delay before a intro talk begins in each scene

---
+++ Assets/Scripts/IntroTalk.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroTalk : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    AudioClip audioClip;
+
+    [SerializeField]
+    float DelayInSeconds = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitThenPlay(DelayInSeconds));
+    }
+
+    IEnumerator WaitThenPlay(float Wait)
+    {
+        yield return new WaitForSeconds(Wait);
+
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+}
